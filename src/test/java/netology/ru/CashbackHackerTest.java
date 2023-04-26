04fedde738e2f6324ddce89a90721fa9105cc470
Fikret <tardileo1996@gmail.com>
2023-04-26 18:04:23 +0300

    commit to JUnit4

---
+++ src/test/java/netology/ru/CashbackHackerTest.java
@@ -0,0 +1,51 @@
+package netology.ru;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackerTest {
+    CashbackHacker cashback = new CashbackHacker();
+
+
+    @Test
+    public void shouldCalculateIfAmountCorrect() {
+
+        int amount = 1700;
+        int actual = cashback.remain(amount);
+        int expected = 300;
+
+        assertEquals(actual, expected);
+
+    }
+
+
+    @Test
+    public void shouldCalculateIfAmountZero() {
+        int amount = 0;
+        int actual = cashback.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+
+    }
+
+
+    @Test
+    public void shouldCalculateIfAmount999() {
+        int amount = 999;
+        int actual = cashback.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateIfAmount1000() {
+        int amount = 1000;
+        int actual = cashback.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+
+}
